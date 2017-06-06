fc1ea1b989842f518936e78dc2415fe054c4cef7
Pedro Câmara <camara-pedro@hotmail.com>
2017-06-06 18:43:26 +0100

    Fix

--- migrations/20170606021403_route-storage.js
+++ migrations/20170606021403_route-storage.js
@@ -18,7 +18,7 @@ exports.up = function up(knex) {
     finalEdge INTEGER,
     help BOOLEAN,
     path INTEGER[],
-    numKnowRoutes VARCHAR(3)
+    numKnownRoutes VARCHAR(3)
   );`)
   .raw(`CREATE OR REPLACE FUNCTION insertCompletedSurvey(json_obj json) RETURNS VOID AS $BODY$
   DECLARE
@@ -28,7 +28,7 @@ exports.up = function up(knex) {
     SELECT * FROM json_to_record(json_obj->'form') AS (age int, experience int, gender gender_t, birth varchar, locality varchar)
     RETURNING id INTO survey_id;
   
-    INSERT INTO surveys_routes(survey_id, initialEdge, finalEdge, help, path, numKnowRoutes)
+    INSERT INTO surveys_routes(survey_id, initialEdge, finalEdge, help, path, numKnownRoutes)
     SELECT survey_id, "initialEdge", "finalEdge", help, path_array AS path, "numKnownRoutes"
     FROM json_array_elements(json_obj->'routes') routes
       CROSS JOIN json_to_record(routes) AS ("initialEdge" int, "finalEdge" int, help boolean, path json, "numKnownRoutes" varchar),
